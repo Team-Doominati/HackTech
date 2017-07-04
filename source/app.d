32df792cc0f4dc2c09cd1a08a695f9beb7042fad
Graham <marrub.xz@gmail.com>
2017-07-04 06:04:39 -0400

    Implement basic OpenGL stuff

--- source/app.d
+++ source/app.d
@@ -3,12 +3,10 @@ import std.string;
 import std.traits;
 
 import derelict.sdl2.sdl;
-import derelict.sdl2.image;
-import derelict.sdl2.ttf;
-import derelict.util.exception;
+import derelict.opengl;
 
-static SDL_Window   *window;
-static SDL_Renderer *renderer;
+private SDL_Window*   window;
+private SDL_GLContext context;
 
 /// Thrown on successfully exiting the application.
 class SuccessfulExit : Exception
@@ -23,26 +21,22 @@ int main()
 {
     initBindings();
     initSDL();
-
-    SDL_Event event;
+    initGL();
 
     try
     {
         for (;;)
         {
-            while (SDL_PollEvent(&event))
+            for (SDL_Event event; SDL_PollEvent(&event);)
             {
-                // Exit event
                 if (event.type == SDL_QUIT)
                     throw new SuccessfulExit();
 
-                // Debug event logging
-                logSDLEvent(event);
+                event.logSDLEvent();
             }
 
-            // Draw
-            SDL_RenderClear(renderer);
-            SDL_RenderPresent(renderer);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            SDL_GL_SwapWindow(window);
         }
     }
     catch (SuccessfulExit)
@@ -58,7 +52,6 @@ void logln(string func = __FUNCTION__, T...)(T args)
 
 void initBindings()
 {
-    // Load Derelict modules
     logln("Loading sdl2...");
     DerelictSDL2.load();
 
@@ -70,24 +63,50 @@ void initBindings()
     DerelictSDL2ttf.load();
     */
 
-    writeln("Done");
+    logln("Loading gl3...");
+    DerelictGL3.load();
+}
+
+void initGL()
+{
+    logln("Loading OpenGL function pointers...");
+    DerelictGL3.reload();
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glEnable(GL_CULL_FACE);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glClearColor(1, 0, 1, 1);
+
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(0, 640, 480, 0, -1, 1);
 }
 
 void initSDL()
 {
-    // Init SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
         throw new Exception("Error initializing SDL");
 
-    // Create Window
-    window = SDL_CreateWindow(toStringz("HackTech"), 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    logln("Initializing window...");
+    window = SDL_CreateWindow("HackTech", 100, 100, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (!window)
         throw new Exception("Error creating SDL window");
 
-    // Create Renderer
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (!renderer)
-        throw new Exception("Error creating SDL renderer");
+    logln("Creating OpenGL context...");
+    context = SDL_GL_CreateContext(window);
+
+    if (SDL_GL_SetSwapInterval(-1) < 0)
+        SDL_GL_SetSwapInterval(1);
+
+    SDL_version ver;
+    SDL_GetVersion(&ver);
+
+    logln("Initialized SDL ", ver.major, ".", ver.minor, ".", ver.patch);
 }
 
 void logSDLEvent(SDL_Event event)
@@ -95,12 +114,14 @@ void logSDLEvent(SDL_Event event)
     switch (event.type)
     {
     case SDL_KEYDOWN, SDL_KEYUP:
-        writeln("SDL Keyboard Event: ", event.key);
+        logln("SDL Keyboard Event: ", event.key);
         break;
     case SDL_MOUSEMOTION:
-        writeln("SDL Mouse Event: ", event.motion);
+        logln("SDL Mouse Event: ", event.motion);
         break;
     default:
         break;
     }
 }
+
+// EOF
